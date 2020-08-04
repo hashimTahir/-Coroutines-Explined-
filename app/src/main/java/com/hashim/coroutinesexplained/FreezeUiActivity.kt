eb369ab713fc93584de96827e8b5722813208772
hashim <hashimtahir@live.com>
2020-08-04 20:09:39 +0500

    Launching 100000 jobs in main thread. with delay of 3000
    Signed-off-by: hashim <hashimtahir@live.com>

--- app/src/main/java/com/hashim/coroutinesexplained/FreezeUiActivity.kt
+++ app/src/main/java/com/hashim/coroutinesexplained/FreezeUiActivity.kt
@@ -4,12 +4,50 @@
 
 package com.hashim.coroutinesexplained
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_freeze_ui.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import timber.log.Timber
 
 class FreezeUiActivity : AppCompatActivity() {
+    var hCount = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_freeze_ui)
+
+        hMain()
+
+        hClickMeB.setOnClickListener {
+            hDetialTv.text = (hCount++).toString()
+        }
+    }
+
+    private suspend fun hFakeNetworkRequest() {
+        Timber.d("Starting Network request")
+        delay(3000)
+        Timber.d("Finished Network request")
+
+    }
+
+    private fun hMain() {
+
+        CoroutineScope(Main).launch {
+            /*Delay only delays the this current job running on ui thread
+            * whereas thread.sleep will freeze the ui thread*/
+            Timber.d("Current thread ${Thread.currentThread().name}")
+//            delay(3000)
+//            Thread.sleep(3000)
+
+            for (i in 1..100_000) {
+                launch {
+                    hFakeNetworkRequest()
+                }
+
+            }
+        }
     }
 }
