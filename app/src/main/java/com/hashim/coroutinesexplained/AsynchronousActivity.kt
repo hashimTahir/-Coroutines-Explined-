394f398e82fae07257e01db5e0b3b7ab75204a72
hashim <hashimtahir@live.com>
2020-08-04 14:32:55 +0500

    Method execution with time taken.
    Signed-off-by: hashim <hashimtahir@live.com>

--- app/src/main/java/com/hashim/coroutinesexplained/AsynchronousActivity.kt
+++ app/src/main/java/com/hashim/coroutinesexplained/AsynchronousActivity.kt
@@ -6,11 +6,72 @@ package com.hashim.coroutinesexplained
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_asynchronous.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.system.measureTimeMillis
 
 class AsynchronousActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_asynchronous)
+        hSetupView()
+    }
+
+    private fun hSetupView() {
+        hClickMeB.setOnClickListener {
+            hSetTextToTv("Button Clicked")
+            hFakeApiRequest()
+        }
+    }
+
+    private fun hFakeApiRequest() {
+        CoroutineScope(IO).launch {
+            var launch1 = launch {
+                val hTime1 = measureTimeMillis {
+                    val hGetResultFromApi1 = hGetResultFromApi1()
+                    hSetTextOnMain("Result From api 1 $hGetResultFromApi1")
+                }
+                hSetTextOnMain("Time for job 1 $hTime1")
+
+
+            }
+            var launch2 = launch {
+                val hTime2 = measureTimeMillis {
+                    val hGetResultFromApi2 = hGetResultFromApi2()
+                    hSetTextOnMain("Result From api 2 $hGetResultFromApi2")
+                }
+                hSetTextOnMain("Time for job 2 $hTime2")
+            }
+
+        }
+    }
+
+
+    private fun hSetTextOnMain(hText: String) {
+        CoroutineScope(Main).launch {
+            hSetTextToTv(hText)
+        }
+    }
+
+    private fun hSetTextToTv(hText: String) {
+        val text = hDetialTv.text.toString()
+        hDetialTv.text = text + "\n$hText"
+    }
+
+    private suspend fun hGetResultFromApi1(): String {
+        delay(1000)
+        return Constants.H_RESULT_1
+
+    }
+
+    private suspend fun hGetResultFromApi2(): String {
+        delay(1700)
+        return Constants.H_RESULT_2
+
     }
 
 }
