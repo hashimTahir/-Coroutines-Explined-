ea9b97fae6d259da73da00b9916bbb33790b08fc
hashim <hashimtahir@live.com>
2020-08-04 22:58:08 +0500

    example code added.
    Signed-off-by: hashim <hashimtahir@live.com>

--- app/src/main/java/com/hashim/coroutinesexplained/GlobalScopeExampleActivity.kt
+++ app/src/main/java/com/hashim/coroutinesexplained/GlobalScopeExampleActivity.kt
@@ -4,12 +4,52 @@
 
 package com.hashim.coroutinesexplained
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_global_scope_example.*
+import kotlinx.coroutines.*
+import timber.log.Timber
 
 class GlobalScopeExampleActivity : AppCompatActivity() {
+    lateinit var hJob: Job
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_global_scope_example)
+        hMain()
+
+        hClickMeB.setOnClickListener {
+            hCancelJob()
+        }
+    }
+
+    private fun hCancelJob() {
+        hJob.cancel()
+    }
+
+    /*Cancelling a parent job cancels all children jobs*/
+    private fun hMain() {
+        val hCurrentTime = System.currentTimeMillis()
+        Timber.d("Starting Parent Job")
+        hJob = CoroutineScope(Dispatchers.Main).launch {
+            launch(Dispatchers.Default) { hStartWork(1) }
+            launch(Dispatchers.Default) { hStartWork(2) }
+            launch(Dispatchers.Default) { hStartWork(3) }
+        }
+        hJob.invokeOnCompletion {
+            if (it != null) {
+                Timber.d("Exception ${it?.message} job was cancelled after ${System.currentTimeMillis() - hCurrentTime}")
+            } else {
+                val hEndTime = System.currentTimeMillis()
+                val hTotalTime = hEndTime - hCurrentTime
+
+                Timber.d("Done in $hTotalTime")
+            }
+        }
+    }
+
+    private suspend fun hStartWork(number: Int) {
+        delay(3000)
+        Timber.d("Work $number being done on ${Thread.currentThread().name}")
+
     }
 }
